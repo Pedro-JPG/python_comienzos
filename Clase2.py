7ffaf221cfa9aae0ff411d98469739d359e3fc4f
Pedro_JPG <32212279+Pedro-JPG@users.noreply.github.com>
2023-08-04 18:14:31 -0300

    Create Clase2.py

---
+++ Clase2.py
@@ -0,0 +1,15 @@
+#1) Mensaje Personalizado:
+#Crea una variable que almacene tu nombre y otra variable que almacene un mensaje de saludo. Luego, muestra el mensaje completo.
+
+
+
+
+#2) Cálculo Simple:
+#Crea una variable para representar tu edad y otra variable para representar la cantidad de años en el futuro. Calcula y muestra la edad que tendrás en ese futuro imaginario.
+
+
+
+
+
+#3) Mensaje con Variables:
+#Crea una variable para almacenar una parte de un mensaje y otra variable para almacenar otra parte. Luego, combina las variables para mostrar un mensaje completo.
