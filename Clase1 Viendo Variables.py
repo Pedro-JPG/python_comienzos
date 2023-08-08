8e57814c0b60cce1beea22034f23773dadd09ff7
Pedro_JPG <32212279+Pedro-JPG@users.noreply.github.com>
2023-08-08 16:47:32 -0300

    Add files via upload

---
+++ Clase1 Viendo Variables.py
@@ -0,0 +1,21 @@
+manzana = 200
+pera = 19.5
+banana = 3*2
+total = manzana + pera + banana
+# print("El total es", total)
+
+
+# Numericos - int
+nro_dia = 4
+#Tipo texto "String" - str
+nombre = "Pedro"
+#Decimal "Float" - float
+valor_remera = 500.99
+#Booleanos "True/False"
+es_mayor_edad = True
+
+#Calculo del suedo
+sueldo = 40000
+descuentos = sueldo * 0.3
+total = sueldo - descuentos
+print("mi sueldo liquido es", total)
