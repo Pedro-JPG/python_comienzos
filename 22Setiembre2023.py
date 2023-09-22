de1cb161369832f6a70f81a3393898bb8fef13e8
Pedro_JPG <32212279+Pedro-JPG@users.noreply.github.com>
2023-09-22 19:52:33 -0300

    Create 22Setiembre2023.py

---
+++ 22Setiembre2023.py
@@ -0,0 +1,6 @@
+notas = [8,7,6,5,9,12,11,12,10,8]
+promedio = 0
+for valor in notas:
+    promedio = promedio + valor
+print("Este es la suma del promedio", promedio)
+print("Este es el promedio", promedio/10)
